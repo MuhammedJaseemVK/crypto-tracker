08560641ab633296b5c069b244d9fdd450bfa252
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-11 14:58:59 +0530

    added live search

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function Crypto({crypto}) {
-    return (
-        <div className='flex justify-between items-center w-full px-3 py-2 bg-red-500 border-b-2 border-bg-white'>
-        <img src={crypto.icon} className='w-7' alt={crypto.symbol} />
-        <p>{crypto.id}</p>
-        <p>{crypto.symbol}</p>
-        <p>{crypto.price}</p>
-        <p>{crypto.priceChange1h}</p>
-        <p>{crypto.marketCap}</p>
-      </div>
-    )
+function Crypto({ crypto }) {
+  return (
+    <div className='flex justify-between items-center w-full px-3 py-2 bg-red-500 border-b-2 border-bg-white'>
+      <img src={crypto.icon} className='w-7' alt={crypto.symbol} />
+      <div>{crypto.id}</div>
+      <div>{crypto.symbol}</div>
+      <div>{crypto.price.toFixed(2)}</div>
+      <div>{crypto.priceChange1h.toFixed(2)}</div>
+      <div>{crypto.marketCap.toFixed(2)}</div>
+    </div>
+  )
 }
 
 export default Crypto
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Crypto from './Crypto'
+import React, { useEffect, useState } from 'react';
+import Crypto from './Crypto';
+import { IoMdRefreshCircle } from 'react-icons/io';
 
 function HeroSection() {
-
-  const [response, setResponse] = useState(null);
-  const apiKey= process.env.REACT_APP_API_KEY;
+  const [coins, setCoins] = useState([]);
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [searchTerm,setSearchTerm] =useState('');
+  const [isLoading,setIsLoading] =useState(false)
 
   useEffect(() => {
-    getCrypto();
+    setIsLoading(true)
+    refreshPage();
   }, [])
 
-  const getCrypto = () => {
+  const handleChange =(e)=>{
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredCoins =coins.filter((coin)=>{
+    return coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  const refreshPage = () => {
     const options = {
       method: 'GET',
       headers: {
@@ -18,20 +29,33 @@ function HeroSection() {
         'X-API-KEY': apiKey
       }
     }
-    fetch("https://openapiv1.coinstats.app/coins", options)
+    fetch("https://openapiv1.coinstats.app/coins?limit=10", options)
       .then(res => res.json())
-      .then(res => { setResponse(res.result); console.log(res) })
+      .then(res => { setCoins(res.result); console.log(res);setIsLoading(false) })
       .catch(err => console.error(err))
   }
+
   return (
-    <div className='bg-violet-500 md:max-w-[50%] w-full py-20 flex flex-col justify-center items-center text-white '>
-      {
-        response && response.map((crypto)=>{
-          return <Crypto key={crypto.symbol} crypto={crypto} />
-        })
-      }
+    <div className='h-full w-full bg-slate-800 flex flex-col items-center'>
+      <div className='bg-blue-500 my-5 flex flex-col gap-2 items-center justify-between py-4 md:py-0 px-4 text-lg text-gray-700'>
+        <p className='font-bold text-xl sm:text-xl text-white'>Crypto Price Tracker</p>
+        <div className='flex items-center gap-3'>
+          <input className='rounded-lg text-sm px-2 py-1 focus:outline-none ' value={searchTerm} onChange={handleChange} placeholder='Search Crypto' type="text" id="search" />
+          <IoMdRefreshCircle className='text-white' size={30} onClick={refreshPage} />
+        </div>
+      </div>
+      <div className='bg-yellow-500 h-full w-[50%] flex flex-col justify-center items-center text-white '>
+        {
+          isLoading && <div>Loading</div>
+        }
+        {
+          filteredCoins && filteredCoins.map((crypto) => {
+            return <Crypto key={crypto.symbol} crypto={crypto} />
+          })
+        }
+      </div>
     </div>
   )
 }
 
-export default HeroSection
+export default HeroSection;
